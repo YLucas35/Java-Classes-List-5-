3cc1c8a09c82e0f2d44d54b81f5a08b4f1a9c43a
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-16 10:44:36 -0300

    Create _atividade_3.java

---
+++ _atividade_3.java
@@ -0,0 +1,14 @@
+package Java_Lista_5_Enquanto;
+
+public class _atividade_3 {
+    public static void main(String[] args) {
+        int count;
+
+        count = 0;
+
+        while (count < 98) {
+            count = count + 2 ;
+            System.out.println(count);
+        }
+    }
+}
