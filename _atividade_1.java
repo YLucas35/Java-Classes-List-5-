4d7f2c4bd25cbab44ce0226f7c6308a7b77602c2
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-17 10:38:34 -0300

    Update _atividade_1.java

--- _atividade_1.java
+++ _atividade_1.java
@@ -3,6 +3,7 @@ package Java_Lista_5_Enquanto;
 public class _atividade_1 {
     public static void main(String[] args) {
         int count;
+        
         count = 0;
 
         while (count < 50) {
