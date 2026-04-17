437510d734eede4708560c8ae5a53cae2dfbe822
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-17 10:39:43 -0300

    Update _atividade_4.java

--- _atividade_4.java
+++ _atividade_4.java
@@ -3,10 +3,10 @@ package Java_Lista_5_Enquanto;
 public class _atividade_4 {
     public static void main(String[] args) {
         int count;
-        boolean result;
+        
         count = 51;
 
-        while (count>1) {
+        while (count > 1) {
             count = count - 2;
             System.out.println(count);
         }
