d84e377795602d20314aee773961f374ddbbee24
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-17 11:06:13 -0300

    Create _atividade_6.java

---
+++ _atividade_6.java
@@ -0,0 +1,17 @@
+package Java_Lista_5_Enquanto;
+
+public class _atividade_6 {
+    public static void main(String[] args) {
+        int count, plus, oldplus;
+
+        plus = 0;
+        count = 1;
+
+        while (count <= 10) {
+            oldplus = plus;
+            plus = plus + count;
+            System.out.println(oldplus+"+"+count+"="+plus);
+            count = count + 1;
+        }
+    }
+}
